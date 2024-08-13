9c9e7b3ae2e5b6a8d5ba88c5ed3fe90c7ea528e6
paupr <paupr@Pau_Laptop>
2024-08-13 14:22:21 +0530

    Testing of my first assignment.

---
+++ AutomationExercise/src/test/java/com/qa/testcases/LoginTest.java
@@ -0,0 +1,13 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void loginTest01()
+	{
+		System.out.println("Hi this is Login Page.");	
+	}
+	
+
+}
