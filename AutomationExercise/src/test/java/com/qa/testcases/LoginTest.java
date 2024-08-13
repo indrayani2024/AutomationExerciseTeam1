c7bb5efbad4a694e1e4ed2c20e991f1065931dbe
paupr <paupr@Pau_Laptop>
2024-08-13 14:31:20 +0530

    Changes has been done

--- AutomationExercise/src/test/java/com/qa/testcases/LoginTest.java
+++ AutomationExercise/src/test/java/com/qa/testcases/LoginTest.java
@@ -6,8 +6,13 @@ public class LoginTest {
 	@Test
 	public void loginTest01()
 	{
-		System.out.println("Hi this is Login Page.");	
+		System.out.println("Hi this is first Login Page Test Case.");	
 	}
 	
+	@Test
+	public void loginTest02()
+	{
+		System.out.println("Hi this is Second Login Page Test Case.");	
+	}
 
 }
